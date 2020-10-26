a5e32d0873f63be3f2480375223f8ff7e152c45a
jatozy <52920861+jatozy@users.noreply.github.com>
2020-10-26 17:52:51 +0100

    Feature/create development container (#1)
    * Add development container which holds android studio
    
    * Add first example application
    
    * Add missing files of the sdk
    
    Removed old sdk and installed it again but add the emulator (the reason for the really big files) to the git ignore list
    
    Move application to workspace folder
    
    Add android sdk API level 30 (Android 11)
    
    * Installed emulator and add the image files to the gitignore list
    
    * Add Windows SDK parallel to the Linux SDK.
    
    * Add fist basic project

---
+++ UltimatePlaylist/app/src/main/java/de/jato_engineering/ultimateplaylist/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package de.jato_engineering.ultimateplaylist.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment. Lalalalalala"
+    }
+    val text: LiveData<String> = _text
+}
